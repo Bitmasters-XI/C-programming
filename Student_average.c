2d0ab427b415302119949880ff13d4376030ebbf
WILLY070 <willykavevamuia@gmail.com>
2024-10-31 13:32:28 +0300

    Prompt user to enter variables

--- Student_average.c
+++ Student_average.c
@@ -15,4 +15,14 @@ int main()
         scanf("%s", &admission_no );
         printf("Enter student's marks: \n");
         scanf("%d%d%d", &mark1,&mark2,&mark3);
+         average_mark=(mark1+mark2+mark3)/3;
+
+        /*displaying of the results*/
+        printf("Admission number:%s\n ", admission_no);
+        printf("Surname:%s\n ", stud_surname);
+        printf("Mark1: %d\n ", mark1);
+        printf("Mark2: %d\n ", mark2);
+        printf("Mark3: %d\n ", mark3);
+        printf("Average mark:%f\n ", average_mark);
+
     }}
