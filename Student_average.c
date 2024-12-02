876e3e602452691e2526bb30f219aac9d937d781
Ivy Atieng <146369247+Atieng@users.noreply.github.com>
2024-12-01 22:01:36 -0800

    Update Student Average.c file

--- Student_average.c
+++ Student_average.c
@@ -3,21 +3,21 @@
 
 int main()
 {
-    char surname[1][10],admission_no[1][10],choice='Y';
-    int mark1,mark2,mark3;
+    char admission_no[1] [10];
+    char stud_surname[1] [10];
+    int mark1, mark2, mark3;
     float average_mark;
+    char choice='Y';
+    while (choice == 'Y' || choice=='y'){
+        printf("Enter student's admission number: ");
+        scanf("%s", &admission_no);
+        printf("Enter student's surname: ");
+        scanf("%s", &stud_surname);
+        printf("Enter the students marks\n");
+        scanf("%d%d%d", &mark1, &mark2, &mark3);
+        average_mark=(mark1+mark2+mark3)/3;
 
-    //Displaying of the results
-    while(choice == 'Y'|| choice == 'y'){
-        printf("Enter student's surname: \n");
-        scanf("%s", &surname);
-        printf("Enter student's admission number: \n");
-        scanf("%s", &admission_no );
-        printf("Enter student's marks: \n");
-        scanf("%d%d%d", &mark1,&mark2,&mark3);
-         average_mark=(mark1+mark2+mark3)/3;
-
-        /*displaying of the results*/
+        //displaying of the results
         printf("Admission number:%s\n ", admission_no);
         printf("Surname:%s\n ", stud_surname);
         printf("Mark1: %d\n ", mark1);
@@ -25,4 +25,24 @@ int main()
         printf("Mark3: %d\n ", mark3);
         printf("Average mark:%f\n ", average_mark);
 
-    }}
+
+        //determining and displaying the students Grade
+         if (average_mark>=70)
+            printf("Grade= A\n");
+         else if (average_mark>=60)
+            printf("Grade= B\n");
+         else if (average_mark>=50)
+            printf("Grade= C\n");
+         else if (average_mark>=40)
+            printf("Grade= D\n");
+         else
+           printf("Grade= D\n");
+
+      //asking the user if they want to continue
+
+         printf("Do you want to enter another student's marks? (Y/N): ");
+         scanf(" %c", &choice);
+    }
+
+return{0};
+}
