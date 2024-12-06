c677bf52be35450d050357c20e5f23b129a69f50
BETHWEL <bethwelk85@gmail.com>
2024-12-06 10:24:34 +0300

    added pseudocode for tax program

--- tax.c
+++ tax.c
@@ -23,3 +23,32 @@ int main()
         printf("tax rate:%f\n",tax_rate);
     
 }
+
+
+
+// START
+//     DECLARE variables:
+//         gross_pay (FLOAT), tax_payable (FLOAT), tax_rate (FLOAT), net_pay (FLOAT)
+//         dependants (INTEGER)
+
+//     PRINT "Enter gross pay"
+//     INPUT gross_pay
+
+//     PRINT "Enter dependants"
+//     INPUT dependants
+
+//     // Determine tax rate based on conditions
+//     IF gross_pay > 20000 AND dependants >= 3 THEN
+//         SET tax_rate to 0.25
+//         PRINT "Tax rate:" tax_rate
+//     ELSE IF gross_pay > 20000 AND dependants < 3 THEN
+//         SET tax_rate to 0.35
+//         PRINT "Tax rate:" tax_rate
+//     ELSE IF gross_pay > 10000 AND dependants >= 3 THEN
+//         SET tax_rate to 0.10
+//         PRINT "Tax rate:" tax_rate
+//     ELSE IF gross_pay > 10000 AND dependants < 3 THEN
+//         SET tax_rate to 0.15
+//         PRINT "Tax rate:" tax_rate
+//     END IF
+// END
